dce9bbfec6d146e199619bcb29753c8124247d98
Danny Lin <danny.lin@nyu.edu>
2018-10-10 10:19:05 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Grubber',
+    version='1.0.0',
+    description='',
+    author='Danny Lin',
+    author_email='danny.lin@nyu.edu',
+    license='MIT',
+    url='https://github.com/dannnylin/Grubber',
+    keywords = [ 'fullstack', 'template', 'python', 'react','npm', 'webpack'],
+    requires=['flask']
+)
